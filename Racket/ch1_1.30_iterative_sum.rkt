2990a83d6e974fb34b43f2441642d9cde7826e65
ZhihangZhang <462143015zzh@gmail.com>
2018-07-10 18:03:16 +0800

    iterative sum added

---
+++ Racket/ch1_1.30_iterative_sum.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (sum term a next b) 
+    (define (iter a result)
+        (if (> a b) result
+            (iter (next a) (+ (term a) result))
+        )
+    ) 
+        
+    (iter a 0)
+)
+
+
+
