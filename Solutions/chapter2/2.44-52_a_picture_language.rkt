b248d37c84098a83c4bfa14ada45354c79ff9995
ZhihangZhang <462143015zzh@gmail.com>
2018-08-02 09:58:27 +0800

    Transforming and combining painters added

--- Solutions/chapter2/2.44-52_a_picture_language.rkt
+++ Solutions/chapter2/2.44-52_a_picture_language.rkt
@@ -41,6 +41,60 @@
             (add-vect (scale-vect (xcor-vect v) (edge1-frame frame))
                       (scale-vect (ycor-vect v) (edge2-frame frame))))))
 
+(define (transform-painter painter origin corner1 corner2) 
+    (lambda (frame)
+        (let ((m (frame-coord-map frame))) 
+            (let ((new-origin (m origin)))
+                (painter (make-frame
+                        new-origin
+                        (sub-vect (m corner1) new-origin)
+                        (sub-vect (m corner2) new-origin)))))))
+
+
+(define (flip-vert painter) 
+    (transform-painter painter
+        (make-vect 0.0 1.0) ; new origin 187
+        (make-vect 1.0 1.0) ; new end of edge1 
+        (make-vect 0.0 0.0))) ; new end of edge2
+
+(define (shrink-to-upper-right painter) 
+    (transform-painter
+        painter (make-vect 0.5 0.5)
+        (make-vect 1.0 0.5) (make-vect 0.5 1.0)))
+
+(define (rotate90 painter) 
+    (transform-painter painter
+        (make-vect 1.0 0.0)
+        (make-vect 1.0 1.0)
+        (make-vect 0.0 0.0)))
+
+(define (squash-inwards painter) 
+    (transform-painter painter
+        (make-vect 0.0 0.0)
+        (make-vect 0.65 0.35)
+        (make-vect 0.35 0.65)))
+
+(define (beside painter1 painter2)
+    (let ((split-point (make-vect 0.5 0.0)))
+        (let ((paint-left (transform-painter
+                painter1
+                (make-vect 0.0 0.0)
+                split-point
+                (make-vect 0.0 1.0)))
+            (paint-right
+                (transform-painter
+                painter2
+                split-point
+                (make-vect 1.0 0.0)
+                (make-vect 0.5 1.0))))
+        (lambda (frame) (paint-left frame) (paint-right frame)))))
+
+
+
+
+
+
+;exercises
 
 (define (make-vect xcor ycor)
     (cons xcor ycor))
@@ -171,3 +225,44 @@
 ;                         (make-segment (make-vect .4 0) (make-vect .25 0)) 
 ;                         ))) 
   ;George! 
+
+
+(define (flip-horiz painter) 
+    (transform-painter painter
+        (make-vect 1.0 0.0) ; new origin 187
+        (make-vect 0.0 0.0) ; new end of edge1 
+        (make-vect 1.0 1.0))) ; new end of edge2
+
+
+(define (cc-rotate180 painter) 
+    (transform-painter painter
+        (make-vect 1.0 1.0)
+        (make-vect 0.0 1.0)
+        (make-vect 1.0 0.0)))
+
+(define (cc-rotate270 painter) 
+    (transform-painter painter
+        (make-vect 0.0 1.0)
+        (make-vect 0.0 0.0)
+        (make-vect 1.0 1.0)))
+
+
+(define (below painter1 painter2)
+    (let ((split-point (make-vect 0.0 0.5)))
+        (let ((paint-top (transform-painter
+                painter1
+                split-point
+                (make-vect 1.0 0.5)
+                (make-vect 0.0 1.0)))
+            (paint-bottom
+                (transform-painter
+                painter2
+                (make-vect 0.0 0.0)
+                (make-vect 1.0 0.0)
+                split-point)))
+        (lambda (frame) (paint-top frame) (paint-bottom frame)))))
+
+
+(define (below painter1 painter2)
+    (cc-rotate270 (beside (rotate90 painter1) (rotate90 painter2)))
+)
