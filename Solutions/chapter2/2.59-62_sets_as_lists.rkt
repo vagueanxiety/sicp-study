f38e9bf767504d76a3b86568422d4a8d42a439c7
ZhihangZhang <462143015zzh@gmail.com>
2018-08-06 11:49:01 +0800

    61, 62  added

--- Solutions/chapter2/2.59-62_sets_as_lists.rkt
+++ Solutions/chapter2/2.59-62_sets_as_lists.rkt
@@ -8,7 +8,6 @@
             ((equal? x (car set)) true)
             (else (element-of-set? x (cdr set)))))
 
-
 (define (adjoin-set x set) 
     (if (element-of-set? x set)
         set
@@ -54,3 +53,54 @@
 ; but extra space are used to store duplicates
 ; when there is a considerable nubmer of duplicates in a set, intersection and element-of-set?
 ; takes more time to execute.
+
+
+
+
+
+
+(define (ordered-intersection-set set1 set2) 
+    (if (or (null? set1) (null? set2))
+        '()
+        (let ((x1 (car set1)) (x2 (car set2)))
+            (cond ((= x1 x2) (cons x1 (intersection-set (cdr set1) (cdr set2))))
+            ((< x1 x2) (intersection-set (cdr set1) set2))
+            ((< x2 x1) (intersection-set set1 (cdr set2)))))))
+
+(define (ordered-element-of-set? x set) 
+    (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (ordered-adjoin-set x set) 
+   (if (null? set) (list x)
+     (let ((head (car set)))
+        (cond ((= head x) set)
+            ((> head x) (cons x set))
+            (else (cons head (ordered-adjoin-set x (cdr set)))))
+     )
+   ))
+
+(define (ordered-union-set set1 set2)
+    (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+            (let ((x1 (car set1)) (x2 (car set2)))
+                (cond 
+                ((= x1 x2) (cons x1 (ordered-union-set (cdr set1) (cdr set2))))
+                ((< x1 x2) (cons x1 (ordered-union-set (cdr set1) set2)))
+                ((< x2 x1) (cons x2 (ordered-union-set set1 (cdr set2))))
+                )))
+    ))
+
+;testing
+(newline)
+(display-list (ordered-intersection-set (list 1 2 3 4) (list 4 6 7 9)))
+(display-list (ordered-element-of-set? 1 (list 4 6 7 9)))
+(display-list (ordered-element-of-set? 4 (list 4 6 7 9)))
+(display-list (ordered-adjoin-set 8 (list 4 6 7 9)))
+(display-list (ordered-adjoin-set 8 '()))
+(newline)
+(display-list (ordered-union-set (list 1 2 3 4) (list 4 6 7 9)))
+(display-list (ordered-union-set '() (list 4 6 7 9)))
